043f779b6bc6172c43c2400e472c96877fdeb594
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-06 21:36:42 +0300

    feat: wire config and db connection into main, add health check route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"OrderPay/pkg/config"
+	"OrderPay/pkg/middleware"
+	"OrderPay/pkg/postgres"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	//Вызываем конфиг и бд
+	cfg := config.MustLoad()
+	conn, err := postgres.ConnDB(*cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		cerr := conn.Close()
+		if cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	//Вызов recovery middleware, всегда идет первым,
+	//чтобы ловить панику нижних методов/сервисов
+	r.Use(middleware.Recovery())
+	r.Use(middleware.RequestId())
+	r.Use(middleware.Timeout())
+	r.Use(middleware.Logger())
+	r.GET("/connection", postgres.HealthCheck(conn))
+
+	//TODO: пробросить порт в конфиг
+	r.Run(":8080")
+}
