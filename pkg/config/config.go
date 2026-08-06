20ee30fec660bac7667fe800dfe20fa76778dd37
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-06 17:06:04 +0300

    feat: add yaml/env config loader for DB settings

---
+++ pkg/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+//Реализуем чтения .yml конфига из корня,
+//в go syntax
+
+type DB struct {
+	Host     string `yaml:"host" required:"true"`
+	Port     string `yaml:"port" required:"true"`
+	Username string `yaml:"user" required:"true"`
+	Password string `env:"DB_PASS" required:"true"`
+}
+
+type Config struct {
+	DB DB `yaml:"db"`
+}
+
+//Функция подключения конфига
+
+func NewConfig() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := Config{}
+
+	err = cleanenv.ReadConfig("config/config.yml", &cfg)
+	if err != nil {
+		log.Fatal("Error loading config")
+	}
+	return &cfg
+}
