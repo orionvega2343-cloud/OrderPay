2557b50288bc1fedf12804ddd91f896e04c33669
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-06 21:17:16 +0300

    feat: add timeout middleware

--- pkg/middleware/timeout_middleware.go
+++ pkg/middleware/timeout_middleware.go
@@ -1,1 +1,19 @@
 package middleware
+
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Timeout() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//TODO: пробросить динамечское время из конфига
+		ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+		defer cancel()
+
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
