2df91a6fd702d7e8e5e71c006c65e9ccbf22fb0a
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-06 20:17:18 +0300

    feat: add request-id middleware

--- pkg/middleware/request_id.go
+++ pkg/middleware/request_id.go
@@ -1,1 +1,32 @@
 package middleware
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type ctxKey string
+
+const requestIDKey ctxKey = "request_id"
+
+//Генерирует/пробрасывает сквозной id запроса,
+//если входящий X-Request-ID уже есть - переиспользуем, если нет, создаем новый с помощью uuid, кладем id:
+//в context для пробрасывания вниз по цепочке вызовов и для обработчика внутри сервиса,
+//в заголовок ответа чтобы клиент/поддержка видели id в ответе
+
+func RequestId() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Request.Header.Get("X-Request-Id")
+		if id == "" {
+			id = uuid.NewString()
+		}
+
+		ctx := context.WithValue(c.Request.Context(), requestIDKey, id)
+
+		c.Request = c.Request.WithContext(ctx)
+		c.Header("X-Request-Id", id)
+		c.Next()
+	}
+}
